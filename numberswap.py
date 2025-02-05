ebc0b73254220e2e85b9d9f29a40f2c6c9d60dce
SuyogNe <suyogn78@gmail.com>
2025-02-05 22:33:11 +0545

    num

---
+++ numberswap.py
@@ -0,0 +1,11 @@
+a=input("Enter your name: ")
+b=int(input("Enter a number: "))
+c=int(input("Enter another number: "))
+
+empty=b
+b=c
+c=empty
+
+
+print("value of b = ", b)
+print("value of c = ", c)
