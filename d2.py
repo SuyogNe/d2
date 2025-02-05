1727a91a5fe898eea59701fadaeb5ebc2f718e00
SuyogNe <suyogn78@gmail.com>
2025-02-05 21:55:27 +0545

    py

---
+++ d2.py
@@ -0,0 +1,11 @@
+firstname="Suyog"
+lastname="Neupane"
+Grade=8
+Age=14
+age=float(Age)
+location="Lalitpur, Nepal"
+print("My name is ",firstname," ",lastname)
+print("My age is ",age)
+print("I study in grade ",Grade)
+print("I live in ",location)
+
---
+++ typecasting.py
@@ -0,0 +1,13 @@
+a=33
+b=27.5
+c="suyog"
+d=33+23
+no1=float(a)
+no2=int(b)
+no3=str(a)
+no4=str(d)
+
+print(no1)
+print(no2)
+print(no3)
+print(no4)
